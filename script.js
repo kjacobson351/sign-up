608057f444385619bcb06686a365893c7f37771d
KyleJ <kjacobson351@gmail.com>
2023-06-21 16:08:21 +0900

    add styles and script to bottom button

---
+++ script.js
@@ -0,0 +1,9 @@
+const createAccountButton = document.getElementById("create-account-button")
+
+createAccountButton.addEventListener("click", change);
+
+function change(){
+    createAccountButton.innerText = "OHHH YEAH!"
+   console.log("test")
+}
+
