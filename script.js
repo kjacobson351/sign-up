80b7bb8ed2685f606a1c5ba4dfe1c4a1180206a6
KyleJ <kjacobson351@gmail.com>
2023-06-26 15:24:26 +0900

    format all code

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const createAccountButton = document.getElementById("create-account-button")
 
 createAccountButton.addEventListener("click", change);
 
-function change(){
+function change() {
     createAccountButton.innerText = "OHHH YEAH!"
-   console.log("test")
+    console.log("test")
 }
 
